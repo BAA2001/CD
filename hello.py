1dba8e0d909fd1f41390c7bcce614c013c9222f2
Bilal <abouallalb@gmail.com>
2023-01-23 16:20:26 +0100

    update

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello World")
+def print_hw():
+    print("Hello World")
+
+
+print_hw()
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+from hello import print_hw
+
+
+def test_print():
+    assert print_hw() == "Hello World"
